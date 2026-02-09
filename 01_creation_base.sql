0ad26a11e7ba1e8fe1c537f07d7e8834ed83de51
ABDELDJALIL BENAIFA <77646178+BenaifaAbdeldjalil@users.noreply.github.com>
2026-02-09 13:06:53 +0100

    Create 01_creation_base.sql

---
+++ 01_creation_base.sql
@@ -0,0 +1,15 @@
+-- =========================================================
+-- PARTIE 01 : CRÉATION DE LA BASE DE DONNÉES
+-- =========================================================
+CREATE DATABASE formation_mariadb
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_general_ci;
+
+USE formation_mariadb;
+
+-- EXERCICE
+-- Créer une base ecole_db et vérifier qu’elle est active
+-- CORRECTION
+-- CREATE DATABASE ecole_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- USE ecole_db;
+-- SELECT DATABASE();
