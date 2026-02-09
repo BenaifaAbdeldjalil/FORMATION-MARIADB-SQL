b8f94f0f2f874504c2ac900c5c67e473337525bc
ABDELDJALIL BENAIFA <77646178+BenaifaAbdeldjalil@users.noreply.github.com>
2026-02-09 13:08:11 +0100

    Create 05_insert.sql

---
+++ 05_insert.sql
@@ -0,0 +1,8 @@
+-- =========================================================
+-- PARTIE 05 : INSERTION DE DONNÉES
+-- =========================================================
+INSERT INTO utilisateurs (nom, email) VALUES ('Ali', 'ali@mail.com');
+
+-- EXERCICE : insérer 2 clients
+-- CORRECTION
+-- INSERT INTO clients (nom) VALUES ('Sara'), ('Yassine');
