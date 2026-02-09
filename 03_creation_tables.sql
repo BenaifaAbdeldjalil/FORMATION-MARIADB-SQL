7c572d68fb06ab577dab4b2ecb25cacbd3960069
ABDELDJALIL BENAIFA <77646178+BenaifaAbdeldjalil@users.noreply.github.com>
2026-02-09 13:07:23 +0100

    Create 03_creation_tables.sql

---
+++ 03_creation_tables.sql
@@ -0,0 +1,12 @@
+-- =========================================================
+-- PARTIE 03 : CRÉATION DE TABLES
+-- =========================================================
+CREATE TABLE utilisateurs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50),
+    email VARCHAR(100)
+);
+
+-- EXERCICE : créer table clients
+-- CORRECTION
+-- CREATE TABLE clients (id INT AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(50));
